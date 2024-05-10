032eba840b87361a6d3d012f3f5be64897e99779
Alevinmol <vinay2@byui.edu>
2024-05-09 22:32:33 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string answer = "yes";
+        while (answer == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = -1;
+            for (int numGuess = 1; guess != magicNumber; numGuess++)
+            {
+                Console.WriteLine("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numGuess} guesses");
+                    Console.WriteLine("Do you want to keep playing?");
+                    answer = Console.ReadLine();
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+        }
     }
 }
