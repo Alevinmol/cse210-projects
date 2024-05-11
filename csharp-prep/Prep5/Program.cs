eceec1cd5b74d7a4a59a24211c367a5b2913b1cd
Alevinmol <vinay2@byui.edu>
2024-05-11 17:14:44 -0600

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string user = Console.ReadLine();
+            return user;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number:");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+
+        static int SquareNumber(int x)
+        {
+            int square = x * x;
+            return square;
+        }
+
+        static void DisplayResult(string uName, int uSquareNumber)
+        {
+            Console.WriteLine($"{uName}, the square of your number is: {uSquareNumber}");
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
     }
 }
