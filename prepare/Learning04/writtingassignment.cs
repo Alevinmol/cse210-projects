c7a8be324e0dbf11178a7656166c830ed3a2ac56
Alevinmol <vinay2@byui.edu>
2024-06-15 17:02:16 -0600

    learning4

---
+++ prepare/Learning04/writtingassignment.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class WrittingAssignment : Assignment
+{
+    private string _title;
+
+    public WrittingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWrittingInformation()
+    {
+        string name = GetStudentName();
+        return $"{_title} by {name}";
+    } 
+}
